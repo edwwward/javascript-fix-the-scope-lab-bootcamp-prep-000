ca84bec22b564e008a563416d3c7ee70e671e938
Edward Castillo <edwardcastillo@me.com>
2018-07-16 21:16:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
 function myAnimal() {
-  var myAnimal = "dog";
-  return myAnimal;  
+  return
 }
-
-function yourAnimal() {
-  var yourAnimal = "cat";
-  return yourAnimal;
-}
-
-function add2(n) {
-  var add2 = n + 2;
-  return add2;
-}
-
-function funkyFunction() {
-  return funkyFunction;
-}
-
-var theFunk = "FUNKY!";
